bb4da0e988b16ff5a3657edf942d1097dc73268e
gisellakontaria <gisella.kontaria@gmail.com>
2021-02-04 20:08:06 +0700

    Added GSON dependency. Retrieved and stored token in SharedPreferences.

--- app/src/main/java/com/example/codingchallenge/Dashboard.java
+++ app/src/main/java/com/example/codingchallenge/Dashboard.java
@@ -2,14 +2,21 @@ package com.example.codingchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import com.cotter.app.IdentityManager;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 public class Dashboard extends AppCompatActivity {
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,17 @@ public class Dashboard extends AppCompatActivity {
         String resp = IdentityManager.handleResponse(getIntent());
         if (resp != null) {
             Log.i("Login Response: ", resp);
+            JsonElement jElement = new JsonParser().parse(resp);
+            JsonObject jsonObject = jElement.getAsJsonObject();
+            jsonObject = jsonObject.getAsJsonObject("oauth_token");
+
+            // Get the user access token and store in Shared Preferences
+            String accessToken = jsonObject.get("access_token").getAsString();
+
+            sp = getSharedPreferences("Token", Context.MODE_PRIVATE);
+            sp.edit().putString("Access Token", accessToken);
+
+            // Add code to validate whether token exists in backend below
         }
 
         // Handles error here
@@ -31,7 +49,8 @@ public class Dashboard extends AppCompatActivity {
 
     // Allows user to return to homepage on click
     public void signOut(View view) {
-        Intent in = new Intent(this, MainActivity.class);
-        startActivity(in);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/codingchallenge/MainActivity.java
+++ app/src/main/java/com/example/codingchallenge/MainActivity.java
@@ -19,9 +19,10 @@ public class MainActivity extends AppCompatActivity {
                 "45d59d89-6129-47f1-8bbd-6e0d9725928e");
     }
 
-    // Retrieves Cotter's login form through WebView
-    public void login(View view) {
+    // Retrieves Cotter's email login form through WebView
+    public void register(View view) {
         Cotter.newIdentity(this, "com.example.codingchallenge://auth_callback").login("EMAIL",
                 this, Dashboard.class);
     }
+
 }
